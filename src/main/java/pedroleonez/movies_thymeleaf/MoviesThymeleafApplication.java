86c2797319ced1e0c2344b783d85193895734ae0
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-22 23:56:28 -0300

    initial commit

---
+++ src/main/java/pedroleonez/movies_thymeleaf/MoviesThymeleafApplication.java
@@ -0,0 +1,13 @@
+package pedroleonez.movies_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pedroleonez/movies_thymeleaf/MoviesThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package pedroleonez.movies_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
