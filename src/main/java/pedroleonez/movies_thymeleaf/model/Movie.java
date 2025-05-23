fb474210080fade9967531d3258a443ca43e5eb7
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-23 15:17:00 -0300

    feat: add @Table

--- src/main/java/pedroleonez/movies_thymeleaf/model/Movie.java
+++ src/main/java/pedroleonez/movies_thymeleaf/model/Movie.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "movies")
 public class Movie {
     
     @Id
